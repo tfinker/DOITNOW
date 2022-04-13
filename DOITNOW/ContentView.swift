6d897ef4652dfa435d54781bd594f6ccd499b880
Finlay Kerr <kerrfj@gmail.com>
2022-04-13 11:16:44 +0100

    Initial Commit

---
+++ DOITNOW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DOITNOW
+//
+//  Created by Finlay Kerr on 4/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DOITNOW/DOITNOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOITNOWApp.swift
+//  DOITNOW
+//
+//  Created by Finlay Kerr on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DOITNOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
