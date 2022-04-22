d0df4d0133ec0b1d9c4bf9315d177b3bbbebce54
Default Seadas User <seadas-user@localhost>
2022-04-22 12:24:49 +0100

    BERMAN added weather API BACKEND :-)

---
+++ DOITNOW/LocationManager.swift
@@ -0,0 +1,56 @@
+//
+//  LocationManager.swift
+//  DOITNOW
+//
+//  Created by Madi Berman on 4/22/22.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+
+//Code partially sourced from https://stackoverflow.com/questions/57681885/how-to-get-current-location-using-swiftui-without-viewcontrollers
+//Code partially sourced from https://www.youtube.com/watch?v=kWAWmWZV0n4
+
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+
+    private let locationManager = CLLocationManager()
+    @Published var locationStatus: CLAuthorizationStatus?
+    @Published var lastLocation: CLLocation?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+   
+    
+    var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationStatus = status
+        print(#function, statusString)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        lastLocation = location
+        print(#function, location)
+    }
+}
---
+++ DOITNOW/WeatherService.swift
@@ -0,0 +1,73 @@
+//
+//  WeatherService.swift
+//  DOITNOW
+//
+//  Created by Madi Berman on 4/22/22.
+//
+
+import SwiftUI
+import Foundation
+
+class WeatherService: ObservableObject{
+    @Published var errorMessage: String?
+    @Published var forecast: WeatherData?
+    @Published var condition: String?
+    let locationManager = LocationManager()
+
+    func getWeather(lat: Double, lon: Double){
+        let weatherURL = "https://api.openweathermap.org/data/2.5/onecall?units=metric&exclude=hourly,current,minutely,alerts&appid=1f2a622de40bc9dd2a29420d2bf1f881"
+        let urlString = "\(weatherURL)&lat=\(lat)&lon=\(lon)"
+        print(urlString)
+         
+        guard let url = URL(string: urlString)
+        else { fatalError("Missing URL")}
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print("Request error: ", error)
+                return
+            }
+            guard let response = response as? HTTPURLResponse else { return }
+            
+            if response.statusCode == 200 {
+                guard let data = data else { return }
+                DispatchQueue.main.async {
+                    do {
+                        let decodedData = try JSONDecoder().decode(WeatherData.self, from: data)
+                        self.forecast = decodedData
+                        let id = decodedData.daily[0].weather[0].id
+                        self.condition = self.getConditionName(weatherID: id)
+                        print("WeatherData...\(String(describing: self.forecast))")
+                    } catch let error {
+                        print("Error decoding: ", error)
+                    }
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    func getConditionName(weatherID: Int) -> String{
+        switch weatherID {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud"
+        default:
+            return "cloud"
+        }
+    }
+    
+}
