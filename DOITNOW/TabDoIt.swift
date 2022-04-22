7ca0edf50e4c6e88afcb90e999b9f04496bf3d76
Default Seadas User <seadas-user@localhost>
2022-04-22 12:32:58 +0100

    Added info.plist to ask user for location data

--- DOITNOW/TabDoIt.swift
+++ DOITNOW/TabDoIt.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct TabDoIt: View {
+    
+    @StateObject var locationManager = LocationManager()
+    var id: Int?
+    @StateObject var weatherService = WeatherService()
+    
     var body: some View {
+        
+        if weatherService.errorMessage != nil{
+            Text(weatherService.errorMessage!)
+        }
         
         ZStack(alignment: .top) {
             Background()
             
             Title(title: "DO IT NOW!")
+            
+            if let lat = locationManager.lastLocation?.coordinate.latitude, let lon = locationManager.lastLocation?.coordinate.longitude {
+                VStack{
+                    Text("Weather").onAppear(perform: {self.weatherService.getWeather(lat: lat, lon: lon)})
+                    if let forecastDaily = weatherService.forecast?.daily[0] {
+                        VStack{
+                            Text("\(forecastDaily.weather[0].main)")
+//                            Text("lat: \(lat)")
+//                            Text("lon: \(lon)")
+                            Text("\(forecastDaily.dt)")
+                            Text("\(forecastDaily.temp.day ,specifier: "%.0f")°C")
+                            if let id = forecastDaily.weather[0].id{
+                                Image(systemName: weatherService.getConditionName(weatherID: id))
+                            }
+                        }
+                        
+                    }
+                }
+            }
         }
     }
 }
