4b363af1d661eceb074fd4a79694adaeeb40166e
Default Seadas User <seadas-user@localhost>
2022-04-25 13:59:54 +0100

    fixed API call added some dashboard features

--- DOITNOW/LocationManager.swift
+++ DOITNOW/LocationManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 import CoreLocation
 import Combine
+import MapKit
 
 //Code partially sourced from https://stackoverflow.com/questions/57681885/how-to-get-current-location-using-swiftui-without-viewcontrollers
 //Code partially sourced from https://www.youtube.com/watch?v=kWAWmWZV0n4
@@ -18,7 +19,8 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     @Published var locationStatus: CLAuthorizationStatus?
     @Published var lastLocation: CLLocation?
-
+    @Published var currentCity: String?
+    
     override init() {
         super.init()
         locationManager.delegate = self
@@ -27,7 +29,6 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
     }
 
-   
     
     var statusString: String {
         guard let status = locationStatus else {
@@ -52,5 +53,22 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         guard let location = locations.last else { return }
         lastLocation = location
         print(#function, location)
+    }
+    
+//Code sourced from https://stackoverflow.com/questions/49276052/unable-to-get-city-name-by-current-latitude-and-longitude-in-swift
+    func getCity(lat: Double, lon: Double){
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: lat, longitude:  lon)
+        
+        geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, _) -> Void in
+
+            placemarks?.forEach { (placemark) in
+
+                if let city = placemark.locality
+                {
+                    self.currentCity = city
+                }
+            }
+        })
     }
 }
